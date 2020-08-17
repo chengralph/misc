4a37561edf94b4efa203dd1f881b9625210a645a
ralphc <chengralph97@gmail.com>
2020-08-16 22:28:45 -0400

    updated covid_flush

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
+from class_deck import deck
+
+
+random_deck = deck()
+random_deck.load_deck()
 
 app = Flask(__name__)
 
-all_posts = [
-    {
-            'title': 'Post1',
-            'content': 'Content of post 1'
-    },
+@app.route("/game/play", methods=['GET'])
+def play_game():
+    random_deck.shuffle()
+    card, current_index, last_index = random_deck.draw()
+    index = f'{current_index}/{last_index}'
+    return render_template("play.html", card_value=card, index=index)
 
-    {
-            'title': 'Post2',
-            'content': 'Content of post 2'
-    }
-]
+@app.route("/game/shuffle", methods=['POST'])
+def shuffle_deck():
+    random_deck.shuffle()
+    return jsonify({"status":"Shuffled"})
+
+@app.route("/game/draw", methods=["POST"])
+def draw_deck():
+    card, current_index, last_index = random_deck.draw()
+    return jsonify({"status": card})
+
 
 @app.route("/posts")
 def posts():
