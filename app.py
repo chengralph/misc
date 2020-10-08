5a951071263712b9db04aa4129f609fc833821e3
ralphc <chengralph97@gmail.com>
2020-10-08 17:18:44 -0400

    pushing changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, jsonify
 from class_deck import deck
-
+import time
 
 random_deck = deck()
 random_deck.load_deck()
@@ -11,27 +11,35 @@ app = Flask(__name__)
 def play_game():
     random_deck.shuffle()
     card, current_index, last_index = random_deck.draw()
-    index = f'{current_index}/{last_index}'
-    return render_template("play.html", card_value=card, index=index)
+    return render_template("play.html", card_value=card, current_index=current_index, last_index=last_index)
 
 @app.route("/game/shuffle", methods=['POST'])
 def shuffle_deck():
     random_deck.shuffle()
     card, current_index, last_index = random_deck.draw()
-    index = f'{current_index}/{last_index}'
-    return jsonify({"status":"Shuffled", "value": card, "index": index})
+    return jsonify({"status":"Shuffled", "value": card, "index": current_index})
 
 @app.route("/game/draw", methods=["POST"])
 def draw_deck():
     card, current_index, last_index = random_deck.draw()
-    index = f'{current_index}/{last_index}'
-    return jsonify({"status":"Drew", "value": card, "index": index})
+    return jsonify({"status":"Drew", "value": card, "index": current_index})
 
 @app.route("/game/reverse", methods=["POST"])
 def reverse_draw():
     card, current_index, last_index = random_deck.reverse_draw()
-    index = f'{current_index}/{last_index}'
-    return jsonify({"status":"Reversed", "value": card, "index": index})
+    return jsonify({"status":"Reversed", "value": card, "index": current_index})
+
+@app.route("/game/nicole", methods=['GET'])
+def play_game1():
+    random_deck.shuffle()
+    card, current_index, last_index = random_deck.draw()
+    return render_template("nicole.html", card_value=card, current_index=current_index, last_index=last_index)
+
+@app.route("/game/test", methods=['GET'])
+def play_test():
+    random_deck.shuffle()
+    card, current_index, last_index = random_deck.draw()
+    return render_template("test.html", card_value=card, current_index=current_index, last_index=last_index)
 
 if __name__ == "__main__":
     app.run(debug=True)
